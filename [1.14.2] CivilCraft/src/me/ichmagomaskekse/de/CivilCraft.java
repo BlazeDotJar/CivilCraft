9d3a4fbf202c91716dd0c34308c3c09cfbcfd553
IchMagOmasKekse <46167960+IchMagOmasKekse@users.noreply.github.com>
2019-07-13 05:56:12 +0200

    ADG

--- [1.14.2] CivilCraft/src/me/ichmagomaskekse/de/CivilCraft.java
+++ [1.14.2] CivilCraft/src/me/ichmagomaskekse/de/CivilCraft.java
@@ -14,7 +14,7 @@ import me.ichmagomaskekse.de.permissions.PermissionList;
 public class CivilCraft extends JavaPlugin {
 	
 	/*
-	 *  Dies ist die Lobby Klasse HHHHAAAHAH
+	 *  Dies ist die Lobby Klasse PC
 	 */
 	private static CivilCraft ccraft = null;
 	public static CivilCraft getInstance() { return ccraft; }
