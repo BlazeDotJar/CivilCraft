2cafa0236964b6f63bba95778da9878ccde14b44
IchMagOmasKekse <46167960+IchMagOmasKekse@users.noreply.github.com>
2019-07-13 05:55:05 +0200

    hz

--- [1.14.2] CivilCraft/src/me/ichmagomaskekse/de/CivilCraft.java
+++ [1.14.2] CivilCraft/src/me/ichmagomaskekse/de/CivilCraft.java
@@ -14,7 +14,7 @@ import me.ichmagomaskekse.de.permissions.PermissionList;
 public class CivilCraft extends JavaPlugin {
 	
 	/*
-	 *  Dies ist die Lobby Klasse ne
+	 *  Dies ist die Lobby Klasse HHHHAAAHAH
 	 */
 	private static CivilCraft ccraft = null;
 	public static CivilCraft getInstance() { return ccraft; }
